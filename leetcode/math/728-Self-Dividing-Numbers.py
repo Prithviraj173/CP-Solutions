57883da1b939e43867b1a39e99f753eca57ab20d
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-08-24 02:46:42 +0530

    Move Self-Dividing-Numbers to leetcode/math/728-Self-Dividing-Numbers.cpp

--- leetcode/math/728-Self-Dividing-Numbers.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def selfDividingNumbers(self, left, right):
-        """
-        :type left: int
-        :type right: int
-        :rtype: List[int]
-        """
-        return [number for number in range(left, right+1) if '0' not in str(number) and all((number % int(char) == 0 for char in str(number)))]
