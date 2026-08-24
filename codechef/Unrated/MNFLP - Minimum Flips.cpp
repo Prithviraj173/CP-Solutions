71781af662ad83e1b40129fc5b0cea12c732a2ab
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-08-24 19:16:06 +0530

    Add Minimum Flips (Unrated)

---
+++ codechef/Unrated/MNFLP - Minimum Flips.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) { cout << A[i] << " "; } cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.begin(), a.end());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for( int i=s;i<e;i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    ll sum = 0, pos = 0, neg = 0;
+    for(int num : arr) {
+        sum += num;
+        if(num == 1) pos++;
+        else neg++;
+    }
+    if(sum & 1) {
+        cout << -1 << endl;
+        return;
+    } else cout << abs(pos - (n >> 1)) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
