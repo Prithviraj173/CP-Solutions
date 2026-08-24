43bf4050e3010049e662394c1d91abe058e1c39b
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-08-24 23:21:11 +0530

    Add New Palindrome (800)

---
+++ codeforces/800/1832A - New Palindrome.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) { cout << A[i] << " "; } cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.rbegin(), a.rend());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for( int i=s;i<e;i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.length();
+    bool flag = false;
+    for(int i = 0; i < n / 2; i++) {
+        if(s[i] != s[0]) {
+            flag = true;
+            break;
+        }
+    }
+    if(flag) yes
+    else no
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
