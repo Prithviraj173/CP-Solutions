a5c9207c2819794c8b49d55adf73d584a557b482
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-08-25 23:18:46 +0530

    Add Maximum Sum (1100)

---
+++ codeforces/1100/1832B - Maximum Sum.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) {cout << A[i] << " ";} cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.rbegin(), a.rend());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for(int i = s; i < e; i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    ll n,k;
+    cin >> n >> k;
+    vector<ll> arr(n);
+    for(ll i = 0; i < n; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    vector<ll> pref(n + 1, 0);
+    for(ll i = 0; i < n; i++) pref[i + 1] += pref[i] + arr[i];
+    ll ans = 0;
+    for(ll i = 0; i <= k; i++) {
+        ll sum = pref[n - k + i] - pref[2 * i];
+        ans = max(ans, sum);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
