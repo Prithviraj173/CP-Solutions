fd2ed57da5252ac1e2ea1c9c310af21ee3f7a414
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-08-26 23:26:40 +0530

    Add Contrast Value (1200)

---
+++ codeforces/1200/1832C - Contrast Value.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) {cout << A[i] << " ";} cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.rbegin(), a.rend());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for(int i = s; i < e; i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+    vector<ll> st;
+    for (int i = 0; i < n; ++i) {
+        if (st.empty() || st.back() != arr[i]) {
+            st.push_back(arr[i]);
+        }
+    }
+    int m = st.size();
+    if (m == 1) {
+        cout << 1 << endl;
+        return;
+    }
+    int ans = 2; 
+    for (int i = 1; i < m - 1; ++i) {
+        if ((st[i] > st[i - 1] && st[i] > st[i + 1]) || (st[i] < st[i - 1] && st[i] < st[i + 1])) ans++;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
