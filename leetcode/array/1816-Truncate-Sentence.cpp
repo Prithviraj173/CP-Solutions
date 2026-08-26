ee1cd1e8ccdd08bb23856284c98b1538777ddfdd
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-08-26 05:51:19 +0530

    Add Truncate-Sentence (array)

---
+++ leetcode/array/1816-Truncate-Sentence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count = 0;
+        string ans = "";
+        for(char c : s) {
+            if(c == ' ') {
+                count++;
+                if(count == k) break;
+            }
+            ans += c;
+        }
+        return ans;
+    }
+};
