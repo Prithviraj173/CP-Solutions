123f05538d6925cc205dd1c81ca501207d2b2ffb
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-09-01 23:19:57 +0530

    Add Alice, Bob and Candies (1300)

---
+++ codeforces/1300/1352D - Alice, Bob and Candies.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) {cout << A[i] << " ";} cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.rbegin(), a.rend());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for(int i = s; i < e; i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for(ll i = 0; i < n; i++) cin >> arr[i];
+    ll alice = 0, bob = 0, moves = 0;
+    bool a = true;
+    ll l = 0, r = n - 1, cur = 0, prev = 0;
+    while(l <= r) {
+        moves++;
+        if(a) {
+            while(l <= r && cur <= prev) {
+                cur += arr[l];
+                l++;
+            }
+            alice += cur;
+        } else {
+            while(l <= r && cur <= prev) {
+                cur += arr[r];
+                r--;
+            }
+            bob += cur;
+        }
+        prev = cur;
+        a = !a;
+        cur = 0;
+    }
+    cout << moves << " " << alice << " " << bob << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
