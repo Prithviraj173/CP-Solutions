a905f8e08a45639990be4516885cbf1a8b1d1ac7
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-09-03 03:54:52 +0530

    Add XOR and Triangle (1100)

---
+++ codeforces/1100/2074C - XOR and Triangle.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) {cout << A[i] << " ";} cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.rbegin(), a.rend());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for(int i = s; i < e; i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    ll x;
+    cin >> x;
+    if((x & (x - 1)) == 0 || (x & (x + 1)) == 0) {
+        cout << -1 << endl;
+        return;
+    }
+    int msb = 63 - __builtin_clzll(x);
+    cout << (1LL << msb) - 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
