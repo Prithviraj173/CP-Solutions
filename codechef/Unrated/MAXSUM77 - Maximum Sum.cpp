7d767de927906ab0430c16336dd3cc6a9507fdc5
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-09-02 20:10:51 +0530

    Add Maximum Sum (Unrated)

---
+++ codechef/Unrated/MAXSUM77 - Maximum Sum.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+#define vout(A) for(int i = 0; i < A.size(); i++) {cout << A[i] << " ";} cout << endl;
+#define ll long long
+#define ull unsigned long long
+#define yes cout <<"YES"<<endl;
+#define no cout <<"NO"<<endl;
+#define inc(a) sort(a.begin(), a.end());
+#define rev(a) sort(a.rbegin(), a.rend());
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a * b) / gcd(a, b))
+#define f(i,s,e) for(int i = s; i < e; i++)
+const int MOD = 1e9 + 7;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> arr(n);
+    for(ll i = 0; i < n; i++) cin >> arr[i];
+    ll sum = 0;
+    for(ll i = 0; i < n - k; i++) sum += arr[i];
+    ll ans = sum;
+    for(ll i = n - k; i < n; i++) {
+        sum += arr[i] - arr[i - n + k];
+        ans = max(ans, sum);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
