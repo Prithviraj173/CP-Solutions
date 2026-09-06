c5bc6f480136e2280e37be5c41c46c1a00298183
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-09-06 13:03:23 +0530

    Add Distinct-Subsequences (dynamic-programming)

--- leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
+++ leetcode/dynamic-programming/115-Distinct-Subsequences.cpp
@@ -1,18 +1,16 @@
 class Solution {
-private:
-    int solve(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int no = solve(i + 1, j, s, t, dp);
-        int yes = 0;
-        if(s[i] == t[j]) yes = solve(i + 1, j + 1, s, t, dp);
-        return dp[i][j] = yes + no;
-    }
 public:
     int numDistinct(string s, string t) {
         int n = s.length(), m = t.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(0, 0, s, t, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for(int i = 0; i <= n; i++) dp[i][m] = 1;
+        long long M = 1e9 + 7;
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j];
+                if(s[i] == t[j]) dp[i][j] = (dp[i + 1][j + 1] + dp[i + 1][j]) % M;
+            }
+        }
+        return dp[0][0];
     }
 };
