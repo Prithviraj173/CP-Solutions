8579794897efa3994de79ec1f6207fe338145ac2
Prithviraj <146489412+Prithviraj173@users.noreply.github.com>
2026-09-23 02:53:45 +0530

    Add Check-If-String-Is-a-Prefix-of-Array (two-pointers)

---
+++ leetcode/two-pointers/1961-Check-If-String-Is-a-Prefix-of-Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string res = "";
+        for(int i = 0; i < words.size(); i++) {
+            res += words[i];
+            if(res == s) return true;
+        }
+        return false;
+    }
+};
